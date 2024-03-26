d3462b9d89d731d35bd909f85ba5febbe01a344a
Cadabra <158126064+Caadabra@users.noreply.github.com>
2024-03-27 12:20:39 +1300

    Update scroller.js

--- Day2/JS Scripts/scroller.js
+++ Day2/JS Scripts/scroller.js
@@ -3,13 +3,18 @@ document.addEventListener("scroll", function() {
     var viewportHeight = window.innerHeight;
     var scrollPosition = window.scrollY;
     var scrollPercentage = (scrollPosition / viewportHeight) * 100;
-    
     // Calculate the value of polygon point using 65 (the initial percentage of position) minus the percentage scrolled multiplied by 20
-    var newClipPath = "polygon(0 0, 45% 0, " + (65 - (30 * scrollPercentage / 100)) + "% 100%, 0% 100%, 0% 100%)";
+    var BottomTriPoint = (65 - (30 * scrollPercentage / 100))
+    // Calculate the value of polygon point using 65 (the initial percentage of position) minus the percentage scrolled multiplied by 20
+    var newClipPath = "polygon(0 0, 45% 0, " + (BottomTriPoint) + "% 100%, 0% 100%, 0% 100%)";
     // Apply the new clip path to the triangle element
-    if ((65 - (30 * scrollPercentage / 100)) >= 45) {
+    if (BottomTriPoint >= 45 && scrollPercentage < 90) {
         document.querySelector(".triangle").style.clipPath = newClipPath;
     } else {
         document.querySelector(".triangle").style.clipPath = "polygon(0 0, 45% 0, 45% 100%, 0% 100%, 0% 100%)";
+        console.log(scrollPercentage)
+    }
+    if (scrollPercentage > 120) {
+        document.querySelector(".triangle").style.clipPath = "polygon(0 0, " + ((50 * scrollPercentage / 100)-15) + "% 0, " + ((50 * scrollPercentage / 100)-15) + "% 100%, 0% 100%, 0% 100%)";
     }
 });
